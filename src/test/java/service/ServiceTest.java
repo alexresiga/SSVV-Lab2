74d5389fd5b13067b069894994cbce922dd5859d
alexresiga <resiga.alex@gmail.com>
2020-03-20 15:15:13 +0200

    add files

---
+++ src/test/java/service/ServiceTest.java
@@ -0,0 +1,17 @@
+package service;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class ServiceTest {
+
+
+
+    @Test
+    public void testAddStudentFail() {
+        assertEquals(1, 1);
+
+    }
+}
